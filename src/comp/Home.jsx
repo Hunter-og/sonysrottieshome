7ceea60861b3bafabe003a612e1401fd141ddd94
Hunter-og <floflower436@gmail.com>
2024-09-15 12:46:27 -0700

    update

--- src/comp/Home.jsx
+++ src/comp/Home.jsx
@@ -4,19 +4,18 @@ export default function Home() {
   return (
     <div className=''>
       <div className='bg  '>
-        <div className=' max-w-md  p-6  w-[35%]  h-[80vh] rounded relative text-center  sm:  '>
-          <h1 className='text-[#1eff00] absolute font-bold text- md:text-4xl lg:text-5xl mt-6 font-serif sm:text-[25px]'>
-            SonyrottiesHome
-          </h1>
-          <p className='text-white text-base  lg:text-[15px] absolute mt-[90px] text-center sm: text-[15px] '>
-            Are you on a quest for the perfect Rottweiler companion? Look no further! At SonyrottiesHome.com, we’re your dedicated online resource for connecting with reputable Rottweiler breeders near your location. Our mission is to simplify your journey in finding the ideal Rottweiler puppy for sale near me, ensuring that you bring home a loyal and loving furry friend that perfectly matches your family’s needs. Explore our extensive network of Rottweiler breeders who share our commitment to quality, ethics, and the well-being of these incredible dogs. Whether you’re a seasoned Rottweiler enthusiast or embarking on your first Rottweiler journey, our platform is designed to guide you every step of the way. Discover the joy of Rottweiler ownership with Sonyrottieshome.com, where excellence meets devotion.
-          </p>
-          <div className=' sm:mt-[480px] '>
-            <button className='text-white  bg-[#1eff00] rounded-md py-2 px-4 mt-[200px] sm: '>
-              Available Puppies
-            </button>
-          </div>
-        </div>
+      <div className='max-w-md p-6 w-full md:w-[35%] h-auto md:h-[80vh] rounded relative text-center'>
+  <h1 className='text-[#1eff00] font-bold text-2xl md:text-4xl lg:text-5xl mt-6 font-serif'>
+    SonyrottiesHome
+  </h1>
+  <p className='text-white text-sm md:text-base lg:text-lg mt-4'>
+    Are you on a quest for the perfect Rottweiler companion? Look no further! At SonyrottiesHome.com, we’re your dedicated online resource for connecting with reputable Rottweiler breeders near your location. Our mission is to simplify your journey in finding the ideal Rottweiler puppy for sale near me, ensuring that you bring home a loyal and loving furry friend that perfectly matches your family’s needs. Explore our extensive network of Rottweiler breeders who share our commitment to quality, ethics, and the well-being of these incredible dogs. Whether you’re a seasoned Rottweiler enthusiast or embarking on your first Rottweiler journey, our platform is designed to guide you every step of the way. Discover the joy of Rottweiler ownership with Sonyrottieshome.com, where excellence meets devotion.
+  </p>
+  <button className='text-white bg-[#1eff00] rounded-md py-2 px-4 mt-6 mb-6'>
+    Available Puppies
+  </button>
+</div>
+
       </div>
     </div>
   )
