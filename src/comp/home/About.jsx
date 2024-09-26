e2f187669d74ca69aaa54637cb0a88f564d460fc
Hunter-og <floflower436@gmail.com>
2024-09-26 10:40:14 -0700

    update

--- src/comp/home/About.jsx
+++ src/comp/home/About.jsx
@@ -3,7 +3,22 @@ import React from 'react'
 export default function About() {
   return (
     <div>
-      joiiiiii
+      <div className=' p-[20px]'>
+        <p className=' text-center text-[#3c93f7e5] font-serif font-extrabold mt-[100px] text-[25px]'>ROTTWEILER PUPPIES</p>
+        <h1 className=' text-[#3c93f7e5] font-serif text-[30px] font-extrabold'>About Us</h1>
+        <p className=' mt-[30px] text-[grey]'> Welcome to Sonyrottieshome, a reputable breeder of exceptional Rottweiler puppies. With years of experience and a deep passion for this <br />
+         magnificient  breed, we strive to provide you with the best companions that embody the true essence of the Rottweiler spirit.
+          </p>
+          <p className='  mt-[30px] text-[grey]'>At Rottweiler we believe that every dog deserves a loving and nurtring home. That is why we are committed to breeding healthy,well-tempered,
+            and genetically sound Rottweiler puppies. Our breeding program focuses on maintaining the breed standard  while also <br />
+            prioritizing the overall well-being of our dogs. Our journey as Rottweiler enthusiasts began many years ago when we welcomed our first Rottweiler into our family.
+            <p className='  mt-[30px] text-[grey]'> We were captivated by their loyalty, intelligence, and unwavering devotion from that moment, we knew that we wanted to share the <br />
+            incredible qualities of this breed with others, As dedicated breeder, we have made it our mission to produce Rottweilers that not only excell in confirmation but also possess sound temperaments and exceptional health, Our carefully selected breeding pairs undergo <br />
+            rigorous health testing to ensure that we are producing puppies free from genetic disorders. </p>
+
+          </p>
+          <button className=' rounded-xl text-white bg-[#1eff00cb] py-[10px] px-[20px] mt-[70px] '>Know More</button>
+      </div>
     </div>
   )
 }
--- src/comp/home/Pup.jsx
+++ src/comp/home/Pup.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function pup() {
   return (
     <div>
-      ;uirl fu.
+      
     </div>
   )
 }
